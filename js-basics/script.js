ee8fabb6a3c54551efe354562d1782b081d913e3
psch0rr95 <sebastiannagl@icloud.com>
2020-12-12 13:33:26 +0100

    Tag 2 JS Teil 1

---
+++ js-basics/script.js
@@ -0,0 +1,67 @@
+// console.log('HI. ich funktioniere!');
+
+let text = 'Dies ist meine erste Variablenzuweisung';
+// console.log(text);
+
+//let
+let ursprungsVariable = 'Ursprungstext';
+ursprungsVariable = 'Neuer Text';
+
+// console.log(ursprungsVariable);
+
+//const
+const ersteVariable = 2;
+// ersteVariable = 'Hello';
+// console.log(ersteVariable);
+
+//var
+var variable = 'Dont use me pls';
+// console.log(variable);
+
+// inhalte von variablen - data types
+let testVariable;
+
+//Numbers
+// testVariable = 2;
+// testVariable = 2.222;
+
+//Strings
+// testVariable = 'Hi ich bin der Text Brudi';
+// testVariable = "Hi ich bin der Text Brudi";
+// testVariable = `Hi ich bin der Text Brudi`;
+
+// if (typeof testVariable === 'string') {
+//   testVariable = parseInt(testVariable);
+// }
+
+//boolean (true | false)
+// testVariable = true;
+// testVariable = false;
+
+//truthy || falsy
+// Zahl 0 
+// Schlüsselwort null 
+// Schlüsselwort undefined
+// Boolean false
+// NaN
+// leerer String
+
+// Bedingungen (Conditional Statements)
+
+let a = 1;
+let b = 1;
+
+// ===
+// if (a === b) {
+//   console.log('ja es geht');
+// }
+
+// AND, OR, NOT
+// AND, &&
+
+//OR, ||
+
+//NOT, ! 
+
+// console.log(testVariable);
+// console.log(typeof testVariable);
