378169961312a6577f62d63de2f35956fb1f8898
Katharina Bohn <katharinabohn@KatharisAir2019.fritz.box>
2020-12-12 15:50:49 +0100

    jetzt

---
+++ Aufgabe Matheproblem.js
@@ -0,0 +1,39 @@
+
+let r = 1
+
+for (let r = 1; r<101; r++)
+
+if ((r % 3 == 0)&&(r % 5 == 0)){
+    console.log("fizzbuzz")
+} else
+
+if (r % 3 == 0) {
+    console.log("fizz")
+
+} else
+
+if (r % 5 == 0) {
+    console.log("buzz")
+} else
+
+console.log(r)
+
+g
+
+
+
+ 
+
+
+
+
+//{
+    //console.log(
+        //if(//r durch 3 teilbar fis
+            //r durch 5 teilbar was
+            //weder durch fünf noch drei r
+            //durch fünf und drei fiswas)
+ //   )
+//}
+
+
