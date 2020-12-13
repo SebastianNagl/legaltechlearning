f1f201ef443c9f92f0c8ea333d3ff61ca39ee0c0
psch0rr95 <sebastiannagl@icloud.com>
2020-12-13 13:05:16 +0100

    export update

--- import-export/clientseitig/ergaenzungslieferung.js
+++ import-export/clientseitig/ergaenzungslieferung.js
@@ -1,7 +1,10 @@
-export object = {
+let object = {
   teilnehmer1: 'Moritz',
   teilnehmer2: 'Katharina',
   teilnehmer3: 'Helena',
   teilnehmer4: 'Simon',
   teilnehmer5: 'Hannah'
 }
+
+export { object };
+// export default object;
